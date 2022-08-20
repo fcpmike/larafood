c4011735090aeeb42875da790fed3b320d534c59
Fabiano Morais <fcpm_mike@hotmail.com>
2022-08-20 16:01:22 -0300

    Conclusão da Atualização do Laravel 9

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,7 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Support\Facades\Route;
 
-// use App\Models\Client;
-
-// Route::get('teste', function(){
-//     $client = Client::first();
-//     $token = $client->createToken('token-teste');
-//     dd($token->plainTextToken);
-// });
 Route::prefix('admin')
         ->namespace('Admin')
         ->middleware('auth')
